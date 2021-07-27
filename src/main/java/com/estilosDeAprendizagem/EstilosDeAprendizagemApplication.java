1f6528c58dd024124c3be6da575811bbd0d614bd
Lucas <lucasrufinomaiellaro@gmail.com>
2021-07-27 09:19:07 -0300

    Primeiro commit

---
+++ src/main/java/com/estilosDeAprendizagem/EstilosDeAprendizagemApplication.java
@@ -0,0 +1,13 @@
+package com.estilosDeAprendizagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstilosDeAprendizagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstilosDeAprendizagemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estilosDeAprendizagem/EstilosDeAprendizagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estilosDeAprendizagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstilosDeAprendizagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
